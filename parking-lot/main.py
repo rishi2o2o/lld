896a39cdfc225a9c5f946fb763bd753695190e3b
Rishi-Vakharia1 <rishi.vakharia1@ibm.com>
2026-05-10 00:09:58 +0530

    Minor comments in parking lot code

--- parking-lot/main.py
+++ parking-lot/main.py
@@ -5,22 +5,27 @@ from spot import Spot
 from floor import Floor
 from parking_lot import ParkingLot
 
-# create spots
+# create some parking spots
 floor1_spots = [
     Spot(1, SpotType.SMALL),
     Spot(2, SpotType.MEDIUM),
     Spot(3, SpotType.LARGE)
 ]
+
+# create a floor with those parking spots
 floor1 = Floor(1, floor1_spots)
+
+# create a parking lot with one floor
 lot = ParkingLot([floor1])
 
-# create vehicles
+# create a vehicle to park
 car = Vehicle("KA01AB1234", VehicleType.CAR)
 
-# park car
+# park vehicle
 ticket = lot.park_vehicle(car)
 
-# unpark car
+# unpark vehicle
 lot.unpark_vehicle(ticket.id)
+
 
 
